9b93f1bb25be14538cf81112e8373ec24bfd5557
lucagoc <77584602+lucagoc@users.noreply.github.com>
2025-02-06 16:16:54 +0100

    Spectrogram fonctionnel

--- components/SpectrogramViewer.vue
+++ components/SpectrogramViewer.vue
@@ -12,42 +12,70 @@ export default {
   data() {
     return {
       waterfall: null,
+      width: 800,
+      height: 400,
+      updateInterval: null,
+      dataObj: null,
     };
   },
-  mounted() {
+  mounted() { 
     this.initWaterfall();
-
+    this.startUpdating();
+  },
+  beforeUnmount() {
+    clearInterval(this.updateInterval);
   },
   methods: {
     initWaterfall() {
       const canvas = this.$refs.waterfallCanvas;
-      const width = 800;
-      const height = 400;
       const direction = 'down';
       const options = {
-        lineRate: 30,
+        lineRate: 100,
         colormap: this.generateColormap(),
       };
 
-      const buffer = new Uint8Array(width * height);
-      for (let i = 0; i < buffer.length; i++) {
-        buffer[i] = Math.floor(Math.random() * 256);
-      }
-
-      this.waterfall = new Waterfall({ buffer }, width, height, direction, options);
-      canvas.width = width;
-      canvas.height = height;
-      const ctx = canvas.getContext('2d');
-      ctx.drawImage(this.waterfall.offScreenCvs, 0, 0);
+      this.dataObj = this.getDynamicDataBuffer();
+      this.waterfall = new Waterfall(this.dataObj, this.width, this.height, direction, options);
+      canvas.width = this.width;
+      canvas.height = this.height;
+      // Pas besoin de dessiner ici initialement, la mise à jour le fera
+      // const ctx = canvas.getContext('2d');
+      // ctx.drawImage(this.waterfall.offScreenCvs, 0, 0);
 
       this.waterfall.start();
     },
     generateColormap() {
-      const colormap = [];
-      for (let i = 0; i < 256; i++) {
-        colormap.push([i, 0, 255 - i, 255]);
-      }
-      return colormap;
+      return Array.from({ length: 256 }, (_, i) => [i, 0, 255 - i, 255]);
+    },
+    getDynamicDataBuffer() {
+      const bufferAry = [];
+      bufferAry[0] = new Uint8Array(this.width);
+      bufferAry[1] = new Uint8Array(this.width);
+
+      const genDynamicData = () => {
+        for (let i = 0; i < this.width; i++) {
+          bufferAry[1][i] = Math.floor(Math.random() * 256);
+        }
+        let tmpBuf = bufferAry[0];
+        bufferAry[0] = bufferAry[1];
+        bufferAry[1] = tmpBuf;
+
+        setTimeout(genDynamicData, 100);
+      };
+
+      genDynamicData();
+      return { buffer: bufferAry[0] };
+    },
+    startUpdating() {
+      this.updateInterval = setInterval(() => {
+        this.waterfall.newLine();
+        this.redrawCanvas(); // Ajout de la ligne pour redessiner
+      }, 10);
+    },
+    redrawCanvas() { // Nouvelle méthode pour redessiner le canvas
+      const canvas = this.$refs.waterfallCanvas;
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(this.waterfall.offScreenCvs, 0, 0);
     },
   },
 };
